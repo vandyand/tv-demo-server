14a00873984130c6562b7ea22f2996faaf1982bb
Andrew <vandyand@gmail.com>
2019-09-18 22:57:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = 3001
+
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world from get!')
+    console.log('got something!')
+})
+
+app.post('/', (req, res) => {
+
+})
+
+app.put('/', (req, res) => {
+
+})
+
+app.delete('/', (req, res) => {
+
+})
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
